000d1ad80255fc312530d436dd6b81c558c46155
Alexander <novozale@mail.ru>
2024-01-11 17:11:24 +0300

    Add files via upload

---
+++ ItemFullDelete/ItemFullDelete/ItemFullDelete.Designer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Drawing;
+using System.Reflection;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+
+namespace ItemDelete
+{
+	public partial class ItemFullDelete
+	{
+		#region Activity Designer generated code
+		
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+        [System.Diagnostics.DebuggerNonUserCode]
+		private void InitializeComponent()
+		{
+            this.CanModifyActivities = true;
+            this.ItemFullDeleteCode = new System.Workflow.Activities.CodeActivity();
+            // 
+            // ItemFullDeleteCode
+            // 
+            this.ItemFullDeleteCode.Name = "ItemFullDeleteCode";
+            this.ItemFullDeleteCode.ExecuteCode += new System.EventHandler(this.ItemFullDeleteCode_ExecuteCode);
+            // 
+            // ItemFullDelete
+            // 
+            this.Activities.Add(this.ItemFullDeleteCode);
+            this.Name = "ItemFullDelete";
+            this.CanModifyActivities = false;
+
+		}
+
+		#endregion
+
+        private CodeActivity ItemFullDeleteCode;
+
+    }
+}
---
+++ ItemFullDelete/ItemFullDelete/ItemFullDelete.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Drawing;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Workflow;
+using Microsoft.SharePoint.WorkflowActions;
+
+namespace ItemDelete
+{
+	public partial class ItemFullDelete: SequenceActivity
+	{
+		public ItemFullDelete()
+		{
+			InitializeComponent();
+		}
+
+        #region Properties
+
+        public static DependencyProperty __ContextProperty = System.Workflow.ComponentModel.DependencyProperty.Register("__Context", typeof(WorkflowContext), typeof(ItemFullDelete));
+
+        [Description("Context")]
+        [ValidationOption(ValidationOption.Required)]
+        [BrowsableAttribute(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public WorkflowContext __Context
+        {
+            get
+            {
+                return ((WorkflowContext)(base.GetValue(ItemFullDelete.__ContextProperty)));
+            }
+            set
+            {
+                base.SetValue(ItemFullDelete.__ContextProperty, value);
+            }
+        }
+
+        public static DependencyProperty __ListIdProperty = System.Workflow.ComponentModel.DependencyProperty.Register("__ListId", typeof(string), typeof(ItemFullDelete));
+        [Description("__ListId")]
+        [ValidationOption(ValidationOption.Required)]
+        [BrowsableAttribute(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public string __ListId
+        {
+            get
+            {
+                return ((string)(base.GetValue(ItemFullDelete.__ListIdProperty)));
+            }
+            set
+            {
+                base.SetValue(ItemFullDelete.__ListIdProperty, value);
+            }
+        }
+
+        public static DependencyProperty __ListItemProperty = System.Workflow.ComponentModel.DependencyProperty.Register("__ListItem", typeof(int), typeof(ItemFullDelete));
+        [Description("__ListItem")]
+        [ValidationOption(ValidationOption.Required)]
+        [BrowsableAttribute(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public int __ListItem
+        {
+            get
+            {
+                return ((int)(base.GetValue(ItemFullDelete.__ListItemProperty)));
+            }
+            set
+            {
+                base.SetValue(ItemFullDelete.__ListItemProperty, value);
+            }
+        }
+
+
+        #endregion
+
+
+        private void ItemFullDeleteCode_ExecuteCode(object sender, EventArgs e)
+        {
+            try
+            {
+                SPSecurity.RunWithElevatedPrivileges(delegate() //запуск с повышенными привилегиями (system account)
+                //для того, чтобы это работало, необходимо чтобы списку (элементу)
+                //для system account были назначены полные права (чтение и запись)
+                {
+                    using (SPSite Site = new SPSite(__Context.Site.ID))
+                    {
+                        using (SPWeb spVeb = Site.OpenWeb(GetServerRelUrlFromFullUrl(__Context.Web.Url)))
+                        {
+                            //удаление элемента списка
+                            SPList List = spVeb.Lists[new Guid(this.__ListId)];
+                            SPListItem listItem = List.GetItemById(this.__ListItem);
+                            listItem.Delete();
+                        }
+                    }
+                });
+                return;
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        //Получение относительного URL из полного
+        internal static string GetServerRelUrlFromFullUrl(string url)
+        {
+            int index = url.IndexOf("//");
+            if ((index < 0) || (index == (url.Length - 2)))
+            {
+                throw new ArgumentException();
+            }
+            int startIndex = url.IndexOf('/', index + 2);
+            if (startIndex < 0)
+            {
+                return "/";
+            }
+            string str = url.Substring(startIndex);
+            if (str.IndexOf("?") >= 0)
+                str = str.Substring(0, str.IndexOf("?"));
+
+            if (str.IndexOf(".aspx") > 0)
+                str = str.Substring(0, str.LastIndexOf("/"));
+
+            if ((str.Length > 1) && (str[str.Length - 1] == '/'))
+            {
+                return str.Substring(0, str.Length - 1);
+            }
+            return str;
+        } 
+	}
+}
